59233955aaca1aa8c2bc6cbd121131d8c04fbceb
computercarguy <computercarguy@gmail.com>
2024-04-02 19:57:36 -0700

    Fix React file serving
    Set buildpath for React app
    Simplify the build process

--- server.ts
+++ server.ts
@@ -20,6 +20,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+    next();
+});
+
 app.use(new UtilitiesCreditsRouter().setupRouter());
 app.use(new UtilitiesRouter().setupRouter());
 app.use(new StripeRouter().setupRouter());
@@ -31,13 +39,6 @@ app.use(new HealthRouter().setupRouter());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
-    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
-    next();
-});
 app.use(express.static(__dirname  + '/build'));
 
 app.listen(port, () => {
