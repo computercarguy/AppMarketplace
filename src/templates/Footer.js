99e8e6998f4f013ef4159775ee029e8263a35716
computercarguy <computercarguy@gmail.com>
2024-05-02 10:33:27 -0700

    Added email address to footer

--- src/templates/Footer.js
+++ src/templates/Footer.js
@@ -5,6 +5,8 @@ class Footer extends Component {
         return (
             <div className="center">
                 <small>&copy; Copyright @{new Date().getFullYear()}, Eric's Gear, All Rights Reserved.</small> 
+                <br />
+                <a href="mailto:sales@ericsgear.com?subject=App Marketplace">sales@ericsgear.com</a>
             </div>
         );
     };
