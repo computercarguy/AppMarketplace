231d6e4d2a217911cb4db686afbb74e765660ca8
computercarguy <computercarguy@gmail.com>
2024-11-21 16:56:26 -0800

    Fxi build
    Added checks to health check

--- src/api/repos/health.ts
+++ src/api/repos/health.ts
@@ -3,20 +3,27 @@ import { Request, Response } from "express-serve-static-core";
 import useSendResponse from "../hooks/useSendResponse";
 import { DbWrapper } from '../db/dbWrapper';
 import { ApiResponse } from '../models/ApiResponse';
+import useSecrets from "../hooks/useSecrets";
 
 @Service()
 export class Health {
     private dbWrapper = Container.get(DbWrapper);
 
     async getHealth(req: Request, res: Response) {
-        this.dbWrapper.healthCheck((response: ApiResponse) => {
-            if (response.error) {
-                useSendResponse(res, "App Marketplace is running with issues.", JSON.stringify(response.error));
-                //this.dbWrapper.savelog("health.ts", "getHealth", "healthCheck", null, JSON.stringify(response.error));
-                return;
+        this.dbWrapper.healthCheck(async (response: ApiResponse) => {
+            const hosttype = process.env.hostType;
+            const secret = await useSecrets();
+            const errors:string[] = [];
+
+            if (!secret) {
+                errors.push("Failed to get secret.");
             }
 
-            useSendResponse(res, "App Marketplace is running.", null);
+            if (response.error) {
+                errors.push(JSON.stringify(response.error));
+            }
+
+            useSendResponse(res, `App Marketplace is running. ${hosttype.toUpperCase()}`, errors.join(",\r\n"));
         });
     }
 }
